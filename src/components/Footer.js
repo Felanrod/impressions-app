5dac8a0d66c04fd4d108f849ef42aa178b909f8b
Felanrod <link8801@hotmail.com>
2021-08-02 17:07:41 -0400

    Created Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = ({ content }) => {
+    return (
+        <footer>
+            <p>Copyright 2021 Impressions Custom Upholstery</p>
+            <p id="siteTag">Site created by Joel F Murphy 2021</p>
+        </footer>
+    )
+}
+
+export default Footer
