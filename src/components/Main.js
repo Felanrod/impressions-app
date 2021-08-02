2759dd02b2bc7fabd8f31f29f6e8312a0cdb2196
Felanrod <link8801@hotmail.com>
2021-08-02 16:58:59 -0400

    Updated Main with Other Sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import Section from "./Section"
 import ImageCorralFlex from "./ImageCorralFlex"
+import SectionText from "./SectionText"
 import exampleImage1 from "../assets/images/example1.jpg"
 import exampleImage2 from "../assets/images/example2.jpg"
 import exampleImage3 from "../assets/images/example3.jpg"
@@ -12,6 +13,18 @@ const Main = () => {
                     src2={exampleImage2} alt2={'Example 2'}
                     src3={exampleImage3} alt3={'Example 3'}/>
                     
+                }
+            />
+            <Section sectionId={'intro'} headerId={'introHeader'} header={'Introduction'} sectionContent={
+                    <SectionText content={"This is some filler text to fill some space, couldn't access lorem ipsum generator at the time and didn't want to make it too full"} />
+                }
+            />
+            <Section sectionId={'work'} headerId={'workHeader'} header={'Work'} sectionContent={
+                    <SectionText content={"Filler for Gallery"}
+                }
+            />
+            <Section sectionId={'contact'} headerId={'contactHeader'} header={'Contact'} sectionContent={
+                    <SectionText content={"Filler for Contact"}
                 }
             />
         </main>
